72edc8f011d5661dc451100cf54c76acc34e4ed7
Dipak Bari <dipakbari4@gmail.com>
2022-09-10 22:50:23 +0530

    [Added] Menu for getting object from loop

--- app/src/main/java/com/dipak/recyclercounterstate/MainActivity.kt
+++ app/src/main/java/com/dipak/recyclercounterstate/MainActivity.kt
@@ -2,23 +2,46 @@ package com.dipak.recyclercounterstate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import com.dipak.recyclercounterstate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val products = mutableListOf<Product>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val products = mutableListOf<Product>()
+        val productAdapter = ProductAdapter(products)
+        binding.recyclerView.adapter = productAdapter
 
         for (index in 1..100) {
             products.add(Product("Product #$index", index, 5))
+            productAdapter.notifyItemInserted(index)
         }
+    }
 
-        val productAdapter = ProductAdapter(products)
-        binding.recyclerView.adapter = productAdapter
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_check -> {
+                for (product in products) {
+                    Log.d(TAG, "product $product")
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
